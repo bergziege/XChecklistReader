e8e658efb4303bcb17afd1a386cceb9e3d2a7889
Bernd Boruttau <b.boruttau@web.de>
2020-10-06 20:16:22 +0200

    Added simple view model and bindings(without IOC/VML yet) and some reasharper induced chaos

--- XChecklistReader/XChecklistReader.Shared/MainPage.xaml.cs
+++ XChecklistReader/XChecklistReader.Shared/MainPage.xaml.cs
@@ -1,41 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage;
-using Windows.Storage.Pickers;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
+using XChecklistReader.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace XChecklistReader
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-
-        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
-            FileOpenPicker fileOpenPicker = new Windows.Storage.Pickers.FileOpenPicker();
-            fileOpenPicker.ViewMode = PickerViewMode.List;
-            fileOpenPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
-            fileOpenPicker.FileTypeFilter.Add(".txt");
-            StorageFile file = await fileOpenPicker.PickSingleFileAsync();
-            lblSelectedFile.Text = file.Path;
+        public MainPage() {
+            DataContext = new MainPageViewModel();
+            InitializeComponent();
         }
     }
 }
---
+++ XChecklistReader/XChecklistReader.Shared/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Uno.UI.Common;
+
+
+namespace XChecklistReader.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _selectedFilePath = "42";
+
+        public MainPageViewModel() {
+            SelectFileCommand = new DelegateCommand(SelectFile);
+        }
+
+        public string SelectedFilePath
+        {
+            get => _selectedFilePath;
+            private set {
+                _selectedFilePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DelegateCommand SelectFileCommand { get; }
+
+        private async void SelectFile() {
+            FileOpenPicker fileOpenPicker = new Windows.Storage.Pickers.FileOpenPicker();
+            fileOpenPicker.ViewMode = PickerViewMode.List;
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            fileOpenPicker.FileTypeFilter.Add(".txt");
+            StorageFile file = await fileOpenPicker.PickSingleFileAsync();
+            if (file != null) {
+                SelectedFilePath = file.Path; 
+            }
+        }
+    }
+}
