25028df6ec9940ce5da7760c5bc87034db5fbe1d
Bernd Boruttau <b.boruttau@web.de>
2020-10-08 21:03:52 +0200

    #3 showing simple checklist items in UI. TODO: parse Datarefs into separate Properties

--- XChecklistReader.Services/Domain/Builder/ChecklistBuilder.cs
+++ XChecklistReader.Services/Domain/Builder/ChecklistBuilder.cs
@@ -1,10 +1,25 @@
-﻿namespace XChecklistReader.Services.Domain.Builder {
+﻿using System.Collections.Generic;
+using Uno.Extensions;
+
+namespace XChecklistReader.Services.Domain.Builder {
     public class ChecklistBuilder {
+        private readonly IList<ChecklistItemBase> _items = new List<ChecklistItemBase>();
         private string _menuName;
         private string _name;
 
         public Checklist Build() {
-            return new Checklist(_name, _menuName);
+            if (_items.Empty()) {
+                SimpleCheckItemBuilder itemBuilder = new SimpleCheckItemBuilder();
+                _items.Add(itemBuilder.WithDescription("Check item 1").WithCondition("Down").Build());
+                _items.Add(itemBuilder.WithDescription("Check item 2").Build());
+                _items.Add(itemBuilder.WithDescription("Check item 3").WithCondition("Up").Build());
+            }
+
+            Checklist checklist = new Checklist(_name, _menuName);
+            foreach (var checklistItem in _items) {
+                checklist.AddItem(checklistItem);
+            }
+            return checklist;
         }
 
         public ChecklistBuilder WithName(string name) {
---
+++ XChecklistReader/XChecklistReader.Shared/UI/Main/TemplateSelectors/ChecklistItemTemplateSelector.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using XChecklistReader.Services.Domain;
+
+namespace XChecklistReader.UI.Main.TemplateSelectors {
+    public class ChecklistItemTemplateSelector : DataTemplateSelector {
+
+        public DataTemplate SimpleChecklistItemTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container) {
+            if (item is SimpleChecklistItem) {
+                return SimpleChecklistItemTemplate;
+            }
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
