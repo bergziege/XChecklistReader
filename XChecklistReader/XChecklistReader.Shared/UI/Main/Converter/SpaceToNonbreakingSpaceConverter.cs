0c1ff472f856cc69001eebdcad80baf703b2f6b2
Bernd Boruttau <b.boruttau@web.de>
2020-10-11 19:57:38 +0200

    #6 trying to preserve space characters when binding on textblobk text

---
+++ XChecklistReader/XChecklistReader.Shared/UI/Main/Converter/SpaceToNonbreakingSpaceConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace XChecklistReader.UI.Main.Converter {
+    public class SpaceToNonbreakingSpaceConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            if (value is string text) {
+                return text.Replace(' ', System.Convert.ToChar(160));
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
